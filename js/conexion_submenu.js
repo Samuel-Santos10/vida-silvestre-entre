058302db39ab002a7cb6e78165ce1acac0f0aac2
Carlos <saymonlenny@gmail.com>
2022-11-28 17:11:15 -0600

    ultimo

---
+++ js/conexion_submenu.js
@@ -0,0 +1,335 @@
+function mostrarSeccionUsulutan() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "block";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionSanMiguel() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "block"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionMorazan() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "block"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionLaUnion() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "block"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionSanVicente() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "block"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionSanSalvador() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "block"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionCuscatlan() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "block"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionChalatenando() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "block"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionCabanhas() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "block"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionAhuachapan() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "block"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionLaLibertad() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "block"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionSonsonate() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "block"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionSantaAna() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "block"; // Santa Ana
+  collection[12].style.display = "none"; // La Paz
+}
+
+function mostrarSeccionLaPaz() {
+  const collectionDefecto = document.getElementsByClassName(
+    "containerSeccionPrincipalDefecto"
+  );
+  collectionDefecto[0].style.display = "none";
+
+  const collection = document.getElementsByClassName(
+    "containerSeccionPrincipal"
+  );
+  collection[0].style.display = "none"; //san miguel
+  collection[1].style.display = "none"; //morazan
+  collection[2].style.display = "none"; //la union
+  collection[3].style.display = "none"; //San Vicente
+  collection[4].style.display = "none"; //San Salvador
+  collection[5].style.display = "none"; //Cuscatlan
+  collection[6].style.display = "none"; //Chalatenando
+  collection[7].style.display = "none"; //Cabanhas
+  collection[8].style.display = "none"; //Ahuachapan
+  collection[9].style.display = "none"; //La Libertad
+  collection[10].style.display = "none"; // Sonsonate
+  collection[11].style.display = "none"; // Santa Ana
+  collection[12].style.display = "block"; // La Paz
+}
